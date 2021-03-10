ea658f74ae903570516a0b8779881bc32e9210b0
samreed18 <sreed@stromeros.co.uk>
2021-03-10 13:47:08 +0000

    Commit

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -7,7 +7,7 @@ public class Deck
     private int i;
     private int finalBalance;
     private String scenarios[] = {"\n Well done!\nyou won the lottery!\n","Oh no!\nyou've been robbed!\n","\n "};
-    private int money[] = {100,20,200,350,80,45,1,5,75,50,450,375,205,210,410,715,670,600,720,420};
+    private int money[] = {100,-20,200,350,80,45,1,5,75,50,450,375,205,210,410,715,670,600,720,420};
     private Card cards[];
 
     public Deck(){
